5548849304e4c62d146e17f3869b914acac276f9
AlexSichitiu <asichitiu@gmail.com>
2023-04-25 12:27:27 -0700

    Added connection to mongoDB server.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const session = require('express-session');
-const mongo = require('connect-mongodb-session');
+const mongo = require('connect-mongo');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
+
+const node_session_secret = process.env.SESSION_SECRET;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_PW = process.env.MONGODB_PW;
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+
+const expiryTime = 60 * 60 * 1000; // milliseconds
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -10,11 +19,25 @@ app.listen(port, () => {
     console.log("Now listening on port " + port + ".");
 });
 
+var MongoDBStore = mongo.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_PW}@${mongodb_host}/sessions`,
+    crypto: {secret: mongodb_session_secret}
+});
+
+
+app.use(session({
+    secret: node_session_secret,
+    store: MongoDBStore,
+    saveUnitialized: false,
+    resave: true
+}));
+
+
 app.get('/', (req, res) => {
     res.send("<header>Testing!</header>");
 });
 
 app.get('*', (req, res) => {
     res.status(404);
-    res.send("404: File not found");
-})
+    res.send("<h1>404: File not found</h1>");
+});
