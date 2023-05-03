9a22409103a59ffc9ed60e1913cfdbcfb869ed25
AlexSichitiu <asichitiu@gmail.com>
2023-05-03 16:47:53 -0700

    Added many ejs templates and configured index.js to use them.

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+function sessionAuthentication(req, res, next){
+    if (req.session.authenticated){
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+function adminAuthorization(req, res, next){
+    if (req.session.usertype = "admin"){
+        next();
+    } else {
+        res.redirect('/login')
+    }
+}
+
 const express = require('express');
 const session = require('express-session');
 const mongo = require('connect-mongo');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const joi = require('joi');
+const url = require('url');
+
+var navbarItems = [
+    {name: 'Home', link: '/'},
+    {name: 'Log In', link: '/login'},
+    {name: 'Sign Up', link: '/signup'},
+    {name: 'Members', link: '/members'},
+    {name: 'Admin', link: '/admin'}
+]
+var navbar = [];
 
 const node_session_secret = process.env.SESSION_SECRET;
 const mongodb_user = process.env.MONGODB_USER;
@@ -11,7 +37,6 @@ const mongodb_PW = process.env.MONGODB_PW;
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const mongodb_database = process.env.MONGODB_DATABASE;
-
 const sessionExpiryTime = 60 * 60 * 1000; // milliseconds
 const salt = 10;
 
@@ -28,7 +53,7 @@ var MongoDBStore = mongo.create({
 });
 const MongoClient = require('mongodb').MongoClient;
 const database = new MongoClient(atlasURL + '?retryWrites=true', {useNewUrlParser: true, useUnifiedTopology: true});
-const users = database.db(mongodb_database).collection('users');
+const users = database.db(mongodb_database).collection('users_with_types');
 
 app.use(session({
     secret: node_session_secret,
@@ -37,71 +62,42 @@ app.use(session({
     resave: true
 }));
 
+function populateNavbar(req,res,next) {
+    navbar = [{name: 'Home', link: '/'}];
+    if (req.session.authenticated){
+        navbar.push({name: 'Members', link: '/members'}, 
+                    {name: 'Log Out', link:'/logout'});
+        if (usertype = 'admin'){
+            navbar.push({name: 'Admin', link: '/admin'});
+        }
+    } else {
+        navbar.push({name: 'Log In', link: '/login'}, 
+                    {name: 'Sign Up', link:'/signup'});
+    }
+    next();
+}
+
+app.use("/", populateNavbar);
+
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    var html;
-    if (req.session.authenticated){
-        html = `
-        <form action='/members' method='get'>
-            <button type="submit">Members</button>
-        </form> 
-        <form action='/logout' method='get'>
-            <button type="submit">Log Out</button>
-        </form> 
-        `;
-    } else {
-        html = `
-        <form action='/login' method='get'>
-            <button type="submit">Log In</button>
-        </form>
-        <form action='/signup' method='get'>
-            <button type="submit">Sign Up</button>
-        </form>
-        `;
-    }
-    res.send(html);
+    res.render('index', {navbar: navbar});
 });
 
 app.get('/login', (req, res) => {
-    var html = `
-    <div>Log in below.</div>
-    <form action='/log_user_in' method='post'>
-        <input name='email' type='email' placeholder='Email Address'> 
-        <input name='password' type='password' placeholder='Password'>
-        <input type='submit'>
-    </form>
-    `
-    res.send(html);
+    res.render('login', {navbar: navbar});
 })
 
 app.get('/signup', (req, res) => {
-    var html = `
-    <span>Make a new account!</span>
-    <form action='/register_user' method='post'>
-        <input name='firstname' type='text' placeholder='First Name' style='display:block'>
-        <input name='email' type='email' placeholder='Email Address' style='display:block'> 
-        <input name='password' type='password' placeholder='Password' style='display:block'>
-        <input type='submit' value='Make an Account'>
-    </form>
-    `;
-    res.send(html);
+    res.render('signup', {navbar: navbar});
 })
 
-app.get('/members', (req, res) => {
-    if (req.session.authenticated){
-        var image = '/pet' + Math.floor(Math.random() * 3) + '.jpg';
-        var html = `<div>Welcome ${req.session.firstname}</div>
-            <img src=${image} height='500' width='500'>
-            <form action='/logout' method='get'>
-                <button type="submit">Log Out</button>
-            </form> 
-        `;
-        res.send(html);
-    } else {
-        res.redirect('/')
-    }
+app.get('/members', sessionAuthentication, (req, res) => {
+    res.render('members', {firstname: req.session.firstname, navbar: navbar});
 })
 
 app.get('/logout', (req, res) => {
@@ -109,9 +105,15 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 
+app.get('/admin', sessionAuthentication, adminAuthorization, (req, res) => {
+    // TODO get userList
+    res.render('admin', {users: userList, navbar: navbar});
+    // TODO allow promotion and demotion
+})
+
 app.get('*', (req, res) => {
     res.status(404);
-    res.send("<h1>404: Nothing Here!</h1>");
+    res.render('404', {navbar: navbar});
 });
 
 app.post('/log_user_in', async (req, res) => {
@@ -124,23 +126,22 @@ app.post('/log_user_in', async (req, res) => {
     var validCredentials = schema.validate({email, password});
 
     if (validCredentials.error != null){
-        console.log(validCredentials.error);
-        res.send(validCredentials.error.toString().substring(17)
-            + '</br><a href="/login">Try Again</a>');
+        res.render('tryagain', {error: validCredentials.error.toString().substring(17), link: '/login', navbar: navbar});
     } else {
         var user = await users.find({email: email})
-            .project({email: 1, password: 1, firstname:1, _id: 1})
+            .project({email: 1, password: 1, firstname: 1, usertype: 1, _id: 1})
             .toArray();
         if (user.length != 1){
-            res.send('<div>Account does not exist for this email.</div><a href="/login">Try Again</a>');
+            res.render('tryagain', {error: 'Account does not exist for this email.', link: '/login', navbar: navbar});
         } else if (await bcrypt.compare(password, user[0].password)){
             req.session.authenticated = true;
             req.session.firstname = user[0].firstname;
             req.session.email = email;
             req.session.cookie.maxAge = sessionExpiryTime;
+            req.session.usertype = user[0].usertype;
             res.redirect('/members');
         } else {
-            res.send('<div>Invalid email and password combination.</div><a href="/login">Try Again</a>');
+            res.render('tryagain', {error: 'Incorrect password', link: '/login', navbar: navbar});
         }
     }
 })
@@ -155,24 +156,23 @@ app.post('/register_user', async (req, res) => {
         email: joi.string().max(20).required(),
         password: joi.string().max(20).required(),
     });
-
     var validSubmission = schema.validate({firstname, email, password});
+
     if (validSubmission.error != null){
-        console.log(validSubmission.error);
-        res.send(validSubmission.error.toString().substring(17)
-            + '</br><a href="/signup">Try Again</a>');
+        res.render('tryagain', {error: validCredentials.error.toString().substring(17), link: '/signup', navbar: navbar});
     } else {
         var encrypted_password = await bcrypt.hash(password, salt); 
         await users.insertOne({
             email: email,
             password: encrypted_password,
-            firstname: firstname
+            firstname: firstname,
+            type: 'user'
         })
-        console.log(`Created an account for ${firstname}.`);
         req.session.authenticated = true;
         req.session.firstname = firstname;
         req.session.email = email;
         req.session.cookie.maxAge = sessionExpiryTime;
+        req.session.usertype = 'user';
         res.redirect('/members');
     }
 })
